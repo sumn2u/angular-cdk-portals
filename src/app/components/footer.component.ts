136dc44eaef1e9bd854eeb38801c62c5dee4f0d2
suman <suman.kunwar@daliaresearch.com>
2019-05-26 13:02:29 +0200

    change on the footer date

--- src/app/components/footer.component.ts
+++ src/app/components/footer.component.ts
@@ -6,7 +6,7 @@ import {Component} from '@angular/core';
     <footer class="pt-4 my-md-5 pt-md-5 border-top text-center">
       <div class="row">
         <div class="col-12 col-md">
-          <small class="d-block mb-3 text-muted">Suman Kuwar &copy; 2018</small>
+          <small class="d-block mb-3 text-muted">Suman Kuwar &copy; 2019</small>
         </div>
       </div>
     </footer>
